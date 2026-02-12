3c8163f3d32c1cac121cced6e02f53c285e635df
vanitasis <alandanrian77@gmail.com>
2026-02-12 20:30:23 +0700

    update kelengkapan fitur kalkulator

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,36 +7,78 @@ using namespace std ;
 
 int main() {
 
-    int num1, num2, select ;
+    double num1, num2;
+    int select ;
     
+    while (true)
+    {
+    cout << left ;
     cout << " pilih operasi" << endl;
+    cout << "========================" << endl;
     cout << " 1. Perkalian" << endl;
     cout << " 2. Pembagian" << endl;
     cout << " 3. Penjumlahan" << endl;
     cout << " 4. Pengurang" << endl;
     cout << " 5. Kembali" << endl;
+    cout << " 6. Keluar" << endl;
+    cout << "========================" << endl;
 
-    cout << "masukan bilangan pertama :" << endl;
+    cout << "masukan bilangan pertama :" ;
     cin >> num1;
-    cout << "masukan bilangan kedua : " << endl;
+    cout << "masukan bilangan kedua : ";
     cin >> num2 ;
-    
-    cout << "masukan pilihan :" << endl ;
+
+    cout << "masukan pilihan :" ;
     cin >> select ;
+    cout << "========================" << endl;
+
+    if (select == 6)
+    {
+        break;
+    }
+    
+
+    
 
     switch (select)
     {
     case 1:
         
         cout << "hasil dari perkalian bilangan tersebut :" << num1 * num2 <<endl;
+        cout << "========================" << endl;
+        break;
+
+    case 2:
+        
+        cout << "hasil dari pembagian bilangan tersebut :" << num1 / num2 <<endl;
+        cout << "========================" << endl;
+        break;
+
+    case 3:
+        
+        cout << "hasil dari penjumlahan bilangan tersebut :" << num1 + num2 <<endl;
+        cout << "========================" << endl;
+        break;
+
+    case 4:
+        
+        cout << "hasil dari pengurangan bilangan tersebut :" << num1 - num2 <<endl;
+        cout << "========================" << endl;
         break;
     
+
+    case 5:
+        
+        cout << "mengulang input angka.........." <<endl;
+        cout << "========================" << endl;
+        continue;
+
     default:
         
-        
+        cout << "pilihan tidak valid" ;
         break;
     }
 
-
+}
 
 }
